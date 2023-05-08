a9c9a9dd76cdd41dd0c57754c2984d995bbde3a6
GermanHydrogen <admin@germanhydrogen.org>
2023-05-08 09:27:57 +0200

    Added setting of all auto max controle values

--- pyobs_asi/asicamera.py
+++ pyobs_asi/asicamera.py
@@ -97,7 +97,9 @@ class AsiCamera(BaseCamera, ICamera, IWindow, IBinning, IImageFormat, IAbortable
         self._camera.set_control_value(asi.ASI_FLIP, 0)
         self._camera.set_image_type(asi.ASI_IMG_RAW16)
 
-        self._camera.set_control_value(asi.ASI_AUTO_MAX_EXP, int(16 * 1e6))
+        self._camera.set_control_value(asi.ASI_AUTO_MAX_EXP, int(16 * 1e3))
+        self._camera.set_control_value(asi.ASI_AUTO_MAX_GAIN, 50)
+        self._camera.set_control_value(asi.ASI_AUTO_MAX_BRIGHTNESS, 100)
 
         # enabling image mode
         self._camera.stop_video_capture()
